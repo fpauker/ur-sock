db7b623a3ac594e28bc9bb95d7322427938b2118
Florian <florian.pauker@gmail.com>
2019-05-18 21:32:46 +0200

    inital commit dashboard interface class

---
+++ examples/dashboard.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+require 'socket'        # Sockets are in standard library
+require 'logger'
+require 'uri'
+
+module UR
+
+  class Dash
+    module ConnectionState
+      DISCONNECTED = 0
+      CONNECTED = 1
+      STARTED = 2
+      PAUSED = 3
+    end
+
+    def initialize(host, logger=Logger.new(STDOUT,level: :INFO))
+      host = '//' + host if host !~ /\/\//
+      uri = URI::parse(host)
+      @logger = logger
+      @hostname = uri.host
+      @port = uri.port.nil? ? 29999 : uri.port
+      @conn_state = ConnectionState::DISCONNECTED
+      @sock = nil
+    end
+
+    def connect
+      return if @sock
+      @sock = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+      @sock.setsockopt Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1
+      @sock = TCPSocket.new(@hostname, @port)
+      @conn_state = ConnectionState::CONNECTED
+      self
+    end
+
+    def connected?
+      @conn_state != ConnectionState::DISCONNECTED
+    end
+
+    def disconnect
+      if @sock
+        @sock.close
+        @sock = nil
+        @conn_state = ConnectionState::DISCONNECTED
+        @logger.info "Connection closed " + @hostname + ":" + @port.to_s
+      end
+    end
+
+    def play
+      @sock.write("play\n")
+      @logger.info "start program"
+      @logger.info @sock.gets
+    end
+
+    def pause
+      @sock.write("pause\n")
+      @logger.info "paused program"
+      @logger.info @sock.gets
+    end
+  end
+
+end
---
+++ examples/test_dash.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require_relative '../lib/ur-sock'
+#require 'ur-sock'
+require_relative 'dashboard'
+
+con     = UR::Dash.new('192.168.56.101').connect
+puts con.connected?
+con.play
+sleep 10
+con.pause
+sleep 5
+con.play
+### Set Speed to very slow
+# speed_names, speed_types = conf.get_recipe('speed')
+# speed = con.send_input_setup(speed_names, speed_types)
+# speed["speed_slider_mask"] = 1
+# speed["speed_slider_fraction"] = 0
+# con.send(speed)
+con.disconnect
